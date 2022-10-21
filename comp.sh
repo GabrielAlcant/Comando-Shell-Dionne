b9605745da223d33c03079e98064b7945b9bc9d3
Gabriel Ribeiro <77732873+GabrielR123@users.noreply.github.com>
2022-10-21 13:13:51 -0300

    Update comp.sh

--- comp.sh
+++ comp.sh
@@ -1,12 +1,25 @@
-path=/home/sedepti/Documentos/GabrielR/index.html
-path2=/home/sedepti/Documentos/GabrielR/backup.html
+cd /home/aluno/Downloads/Comando-Shell-Dionne-main/
+path=index.html
+cd /home/aluno/Downloads/Comando-Shell-Dionne-main/
+path2=backup.html
+
 #Verificando se os arquivos foram modificados
-if (cmp -s $path $path2); then
+cd 
+comp1=$(zip -r principal.zip $path)
+comp2=$(zip -r  backup.zip $path2)
+
+#Chave Hash
+chave1=$(sha256sum $comp1 | awk '{print$1}' )
+chave2=$(sha256sum $comp2 | awk '{print$1}' )
+
+
+if [ $chave1 = $chave2 ]; then
     echo "Os arquivos não foram modificados"
 
 else
    echo "Os arquivos foram modificados"
    echo "Enviando email..."
+
 # Enviando Email Caso tenha sido modificado
   sendEmail -f gabriel.alcantara@icen.ufpa.br -t gabrielribeiro969@outlook.com -s cupijo.ufpa.br:587 -u "Verificação de Modificação de Arquivos" -m "Seus arquivos principais foram alterados.Possivel invasão" -xu gabriel.alcantara@icen.ufpa.br -xp 'yurirafael123'
   #-a /arquivo.txt \
@@ -14,6 +27,7 @@ else
   #-xp 'yurirafael'\
   #-o tls=yes
    echo "Email enviado com sucesso"
+
 # Copiando os arquvivos de backup e removendo o modificado
 
 rm -r $path
